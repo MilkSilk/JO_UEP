f75799a00aac266027f9c4c3a7e1b91ea58804c7
MilkSilk <42338877+MilkSilk@users.noreply.github.com>
2020-05-12 14:48:02 +0200

    Selenium test code

---
+++ tests/selenium/test_selenium.py
@@ -0,0 +1,18 @@
+import unittest
+
+from selenium.webdriver.android import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class TestSelenium(unittest.TestCase):
+    @unittest.skip("Nie pyknie na serwezre github, bo nie sciagamy sterownika")
+    def test_selenium(self):
+        driver = webdriver.Chrome(executable_path='/Users/jacja/Downloads/chromedriver')
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        self.assertNotIn("No results found.", driver.page_source)
+        driver.close()
